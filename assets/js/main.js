758764f47a888f39e312134d6995c4d7bed70a3e
Praveen Sajjanapu <sajjanapupraveen76@gmail.com>
2024-03-05 23:20:14 +0100

    new commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: Personal - v2.1.0
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-
-
-*/
 
 
 var typed = new Typed('.typing',{
